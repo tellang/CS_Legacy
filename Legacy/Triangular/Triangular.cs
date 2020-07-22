492f0a95dd4126d0690be9a2670b1dbed6e269ea
tellang <tellang@daum.net>
2020-07-22 17:37:45 +0900

    FrontFrame_Complete

--- Legacy/Triangular/Triangular.cs
+++ Legacy/Triangular/Triangular.cs
@@ -176,5 +176,155 @@ namespace Triangular
             Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
-    
+
+    class BackFrame : BaseFrame
+    {
+        public BackFrame (int xPos, int yPos, int width, int height, int term) : base(xPos, yPos, width, height, term){
+            stat = "Back";
+        }
+
+        public BackFrame (ISequence seq) : base(seq)
+        {
+            stat = "Back";
+        }
+
+        public override void Frame(int xPos, int yPos){
+            base.Frame(xPos, yPos);
+            for(int i=1; i <= inHeight; i++)
+            {
+                Console.SetCursorPosition(xPos + 2, yPos + i);
+                for(int j = 1; j<=inWidth; j++)
+                    Console.Write("▩");
+            }
+        }
+    }
+
+    class FrontFrame : BackFrame, IIdentification
+    {
+        public FrontFrame (int xPos, int yPos, int width, int height, int term) : 
+        base(xPos, yPos, width, height, term)
+        {
+            stat = "Front";
+        }
+
+        public FrontFrame (int xPos, int yPos, int width, int height, int term, params string[] job) : 
+        base(xPos, yPos, width, height, term)
+        {
+            Initialize (ref info, ref job);
+        }
+
+        public FrontFrame(ISequence seq) : base(seq)
+        {
+            stat = "Front";
+        }
+
+        public FrontFrame(ISequence sequence, params string[] job) : this(seq)
+        {
+            Initialize(ref this.info, ref job);
+        }
+
+        public CardInfo[] info = new CardInfo[3];
+
+        public void FrameLabel (int xPos, int yPos, string label)
+        {
+            Console.SetCursorPosition(xPos + (outWidth - label.Length)/2, yPos + outHeight/2);
+            Console.Write(label);
+        }
+
+        public void FrameLabel (int xPos, int yPos, int nth)
+        {
+            Console.SetCursorPosition(xPos +(outWidth - info[nth].job.Length)/2, yPos + outHeight/2);
+            Console.Write(info[nth].job);
+        }
+
+        public virtual void FrameLabel (int nth)
+        {
+            Console.SetCursorPosition(xPos + 2, yPos + nth + 1);
+            Console.Write(info[nth].job);
+        }
+
+        public void FrameLabel (int nth, string label)
+        {
+            FrameLabel(xPos + nextCardXPos * nth, yPos, label);
+        }
+
+        public void SlotLabel (int xPos, int yPos, int nth, string[] label)
+        {
+            Console.SetCursorPosition(xPos +(outWidth - label[nth].Length)/2, yPos + outHeight);
+            Console.Write(label[nth]);
+        }
+
+        public void SlotLabel (int nth, string[] label)
+        {
+            SlotLabel(xPos + nextCardXPos * nth, yPos, nth, label);
+        }
+        
+        public virtual void FrameLabel()
+        {
+            for(int i =0; i < info.Length; i++)
+            {
+                Console.SetCursorPosition(xPos + 2, yPos + i + 1);
+                Console.Write(info[i].job);
+            }
+        }
+
+        public override void Frame(int nth)
+        {
+            base.Frame(nth);
+            lives(xPos + nextCardXPos * nth, yPos, nth);
+            FrameLabel (xPos + nextCardXPos * nth, yPos, nth);
+        }
+
+        public override void Frame()
+        {
+            base.Frame(nth);
+            for(int i = 0; i < info.Length; i++)
+            {
+                Console.SetCursorPosition(xPos + 2, yPos + i + 1);
+                Console.Write(info[i].job);
+            }
+        }    
+
+        public void Resize (string[] nw) //Resize slot's size  
+        {
+            for(int i = 0; i < nw.Length; i++)
+            {
+                if(nw[i].Length > inWidth * 2)
+                {
+                    if((nw[i].Length - inWidth * 2)%2 != 0)
+                        inWidth += ((nw[i].Length - inWidth * 2)/2 +1);
+                    else
+                        inWidth += (nw[i].Length - inWidth * 2)/2;
+                }
+                    
+            }
+        }  
+        public void Initialize (ref CardInfo[] bs, ref string[] nw)
+        {
+            if(nw.Length >= bs.Length)
+                Array.Resize<CardInfo>(ref bs, nw.Length);
+            Resize(nw);
+            outWidth = 2*(2+inWidth);
+            nextCardXPos = outWidth + term;
+            for(int i = 0; i < nw.Length; i++)
+            {
+                bs[i].job = nw[i];
+                bs[i].xPos = xPos + nextCardXPos * i;
+                bs[i].yPos = yPos;
+            }
+        }
+
+        public void Lives (int xPos, int yPos, int nth)
+        {
+            ConsoleColor bc, fc;
+            bc = Console.BackgroundColor;
+            fc = Console.ForegroundColor;
+
+            Effect.SetColor(fore: ConsoleColor.Red, Back: bc);
+            Console.SetCursorPosition(xPos + 2, yPos + inHeight);
+            for(int i = 0; i < info[nth].lives; i++)
+                Console.Write("♡");
+            Effect.SetColor(fore: fc, Back: bc);
+        }
+    }
 }
