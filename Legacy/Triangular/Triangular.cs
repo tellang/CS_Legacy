61bb51401a0b9570cfe79cf1bb8c97a1df922b0c
tellang <tellang@daum.net>
2020-07-22 16:00:59 +0900

    BaseFrame_ Complete

--- Legacy/Triangular/Triangular.cs
+++ Legacy/Triangular/Triangular.cs
@@ -34,17 +34,65 @@ namespace Triangular
         public void SetButton();
     }
     interface IJob { public string job { get; set; } }
-    interface ITerm{ public int term { get; set; }}
-    interface IStatus{public string stat{get; set;}}
-    interface INextCardXPos{public int nextCardXPos { get; set; }}
-    interface IRandom { public bool Random(int percent);}
-    interface IIdentification : IPosition, IStatus{}
-    interface ISequence : IPosition, ITerm, ISize{}
-    interface ICusor{
+    interface ITerm { public int term { get; set; } }
+    interface IStatus { public string stat { get; set; } }
+    interface INextCardXPos { public int nextCardXPos { get; set; } }
+    interface IRandom { public bool Random(int percent); }
+    interface IIdentification : IPosition, IStatus { }
+    interface ISequence : IPosition, ITerm, ISize { }
+    interface ICusor
+    {
         public int index { get; set; }
-        public CursorFrame cur {get; set;}
-        public DeleteCursor dCur {get; set;}
-        public FrontFrame source {get; set;}
+        public CursorFrame cur { get; set; }
+        public DeleteCursor dCur { get; set; }
+        public FrontFrame source { get; set; }
         public void SetCursor(ConsoleKeyInfo button);
+    }
+
+    struct CardInfo : IPosition, IJob
+    {
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        public int lives { get; set; }
+        public bool survival { get; set; }
+        public string job { get; set; }
+    }
+
+    struct MapInfo: IPosition
+    {
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        public string kind {get; set;}
+        public string name {get; set;}
+        public CardInfo[] opp;
+        public bool cleard {get; set;}
+    }
+
+    class BaseFrame : ISequence, IStatus, INextCardXPos
+    {
+        public BaseFrame (int xPos, int yPos, int width, int height, int term)
+        {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            this.inWidth = width;
+            this.inHeight = height;
+            this.outHeight = inHeight + 2;
+            this.outWidth = 2*(2+inWidth);
+            this.term = term;
+            nextCardXPos = outWidth + term;
+            this.stat = "None";
+        }
+
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        public int term {get; set;}
+        public int inWidth { get; set; }
+        public int inHeight { get; set; }
+        public int outWidth { get; set; }
+        public int outHeight { get; set; }
+
+        public string stat {get; set;}
+        public int nextCardXPos { get; set; }
+
     }
 }
